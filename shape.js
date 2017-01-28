5c090c1fa95edd0bbd15f7b8cc962bed9f52802d
druchefavour <druchefavour@gmail.com>
2017-01-28 16:01:51 -0600

    commit to create shape constructor

--- shape.js
+++ shape.js
@@ -3,3 +3,16 @@
 var triangle = require("./triangle.js");
 var square = require("./square.js");
 var pentagon = require("./pentagon.js");
+
+// Create the constructor for the Class Shape: It has the property type
+
+var Shape = function (type) {
+
+	//class property
+	this.type = type;
+}
+// class method
+Shape.prototype.get_type = function () {
+	return this.type;
+	console.log(this.type);
+}
