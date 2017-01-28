0d98c3b0c1031c54313bea19adca3bd31d63a640
druchefavour <druchefavour@gmail.com>
2017-01-28 15:41:19 -0600

    commit scripts that uses require keyword to import all the exports

---
+++ shape.js
@@ -0,0 +1,5 @@
+// Use the require keyword to access all of exports in the triangle, square and pentagon file
+
+var triangle = require("./triangle.js");
+var square = require("./square.js");
+var pentagon = require("./pentagon.js");
