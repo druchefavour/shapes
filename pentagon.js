12bb4b5c1b23589301da3cffc51faf4cb737ae9e
druchefavour <druchefavour@gmail.com>
2017-01-28 23:37:46 -0600

    commit pentagon as prototype of Shape

--- pentagon.js
+++ pentagon.js
@@ -1,4 +1,12 @@
 
+// Create a Pentagon - subclass object
+var Pentagon = function (a, b, c, d, e) {
+  this.a = a;
+  this.b = b;
+  this.c = c;
+  this.d = d;
+  this.e = e;
+}
 
 // Export file to shape.js
-module.exports Pentagon;
+module.exports = Pentagon;
--- shape.js
+++ shape.js
@@ -2,9 +2,9 @@
 
 var triangle = require("./triangle.js");
 var square = require("./square.js");
-// var pentagon = require("./pentagon.js");
+var pentagon = require("./pentagon.js");
 
-// Create the constructor for the Class Shape: It has the property type
+// Create the object for the Class Shape: It has the property type
 
 // Shape -class
 function Shape(type) {
@@ -39,7 +39,7 @@ console.log("Shape Type is: ");
 console.log(newTriangle.get_Type()); // Outputs, Shape type and lengths of the sides.
 
 //=========== THE SQUARE SUBCLASS===========================================
-// Apply shape properties on triangle
+// Apply shape properties on square
 function square(a) {
   Shape.call(this, a); // Call class constructor.
 }
@@ -57,3 +57,23 @@ console.log('Is newSquare an instance of square?', newSquare instanceof square);
 console.log('Is newSquare an instance of Shape?', newSquare instanceof Shape);// true
 console.log("Shape Type is: " );
 console.log(newSquare.get_Type()); // Outputs, Shape type and length of the 4 sides.
+
+//=========== THE PENTAGON SUBCLASS===========================================
+// Apply shape properties on PENTAGON
+function pentagon(a, b, c, d, e) {
+  Shape.apply(this, [a, b, c, d, e]); // Apply class constructor.
+}
+
+// subclass extends class
+pentagon.prototype = Object.create(Shape.prototype);
+pentagon.prototype.constructor = pentagon;
+
+// Create pentagon constructor
+var newPentagon = new pentagon("side 1", "side 2", "side 3", "side 4", "side 5");
+
+// Check whether the prototype works
+console.log("---------PENTAGON -------------")
+console.log('Is newPentagon an instance of pentagon?', newPentagon instanceof pentagon);// true
+console.log('Is newPentagon an instance of Shape?', newPentagon instanceof Shape);// true
+console.log("Shape Type is: " );
+console.log(newPentagon.get_Type()); // Outputs, Shape type and lengths of the 5 sides.
